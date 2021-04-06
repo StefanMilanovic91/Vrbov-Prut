788b6a67d50e4df115f7becd6ac674a13421187e
Stefan Milanovic <stefan1811991@gmail.com>
2021-04-06 22:58:50 +0200

    Work on Contact Form.

--- src/components/UI/Map/Map.js
+++ src/components/UI/Map/Map.js
@@ -1,14 +1,16 @@
-import React, {  useState } from 'react';
+import React from 'react';
 
 
 const Map = () => {
     return (
-        <div class="mapouter">
-            <div class="gmap_canvas">
-                <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Jagodina,%20Lovci&t=&z=11&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+        <div className="Map">
+            <div class="mapouter">
+                <div class="gmap_canvas">
+                    <iframe className="Map__iframe" id="gmap_canvas" src="https://maps.google.com/maps?q=Platarska%20radnja%20Vrbov%20Prut&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
                 </div>
+            </div>
         </div>
-        )
+    )
 }
 
 export default Map
--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Map from '../../UI/Map/Map';
 
 const Contact = () => {
+
+    const [form, setForm] = useState({ name: '', email: '', text: '' });
+
+    const name = (value) => {
+        let patt = /^[a-zA-Z0-9_]+( [a-zA-Z0-9_]+)*$ // /^[A-Z]{1}[A-Za-z]{2,25}$/;
+    
+        if(!patt.test(value.replace(/\s/g,''))){
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    const submit = (e) => {
+        e.preventDefault();
+        console.log(name(form.name)); 
+        
+    }
+
     return (
         <div className="Page Contact">
             <div className="container">
@@ -9,13 +28,42 @@ const Contact = () => {
                 <div className="underline"></div>
             </div>
             <div className="container">
-                <div className="row">
-                    <div className="col-12 col-md-6">
-                        <div className="Contact__map">
-                            <Map />
+                <div className="row flex-column-reverse flex-md-row">
+                    <div className="col-12 col-md-6 pb-5">
+                        <Map />
+                    </div>
+                    <div className="col-12 col-md-6 pb-5">
+                        <div className="Contact__data">
+                            <ul className="Navbar__list">
+                                <li className="Navbar-item">063 / 814 - 68 - 29</li>
+                                <li className="Navbar-item">035 / 275 - 201</li>
+                                <li className="Navbar-item">vrbovprut@gmail.com</li>
+                                <a href='http://www.facebook.com' target="blank" className="Navbar-item">Facebook</a>
+                            </ul>
                         </div>
                     </div>
-                    <div className="col-12 col-md-6"></div>
+                </div>
+                <div className="row justify-content-center">
+                    <div className="col-6">
+                        <div className="Contact__form">
+                            <form>
+                                <h3 className="second-title text-center">Kontakt Forma</h3>
+                                <div className="Contact__form__group">
+                                    <label className="Contact__form__label">Ime i prezime</label>
+                                    <input onChange={(e) => setForm({...form, [e.target.name]: e.target.value})} name="name" value={form.name} type="text" className="Contact__form__group__input" placeholder="Ime i prezime"/>
+                                </div>
+                                <div className="Contact__form__group">
+                                    <label className="Contact__form__label">Email adresa</label>
+                                    <input onChange={(e) => setForm({...form, [e.target.name]: e.target.value})} name="email" value={form.email} type="email" className="Contact__form__group__input" placeholder="Email adresa"/>
+                                </div>
+                                <div className="Contact__form__group">
+                                    <label className="Contact__form__label">Text poruke</label>
+                                    <textarea onChange={(e) => setForm({...form, [e.target.name]: e.target.value})} name="text" value={form.text} type="text" className="Contact__form__group__input-text" cols="30" rows="10" placeholder="Vaša poruka"></textarea>
+                                </div>
+                                <button onClick={submit} className="btn btn-block btn-success">Pošalji poruku</button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
