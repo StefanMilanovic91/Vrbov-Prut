8cf25d78ced2f05b01508e0c7b5917de69a06265
Stefan Milanovic <stefan1811991@gmail.com>
2021-04-08 12:43:02 +0200

    Contact Form is finished

--- src/components/UI/Map/Map.js
+++ src/components/UI/Map/Map.js
@@ -4,9 +4,9 @@ import React from 'react';
 const Map = () => {
     return (
         <div className="Map">
-            <div class="mapouter">
-                <div class="gmap_canvas">
-                    <iframe className="Map__iframe" id="gmap_canvas" src="https://maps.google.com/maps?q=Platarska%20radnja%20Vrbov%20Prut&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+            <div className="mapouter">
+                <div className="gmap_canvas">
+                    <iframe className="Map__iframe" id="gmap_canvas" src="https://maps.google.com/maps?q=Platarska%20radnja%20Vrbov%20Prut&t=&z=13&ie=UTF8&iwloc=&output=embed" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"></iframe>
                 </div>
             </div>
         </div>
--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -1,24 +1,113 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Map from '../../UI/Map/Map';
 
 const Contact = () => {
 
-    const [form, setForm] = useState({ name: '', email: '', text: '' });
+    const [form, setForm] = useState({
+        firstName: {
+            value: '',
+            name: 'firstName',
+            isValid: false,
+            isRequired: true,
+            isTouched: false
+        },
+        lastName: {
+            value: '',
+            name: 'lastName',
+            isValid: false,
+            isRequired: true,
+            isTouched: false
+        },
+        email: {
+            value: '',
+            name: 'email',
+            isValid: false,
+            isRequired: true,
+            isTouched: false
+        },
+        text: {
+            value: '',
+            name: 'text',
+            isValid: false,
+            isRequired: true,
+            isTouched: false
+        },
+        isValid: false
+    });
 
-    const name = (value) => {
-        let patt = /^[a-zA-Z0-9_]+( [a-zA-Z0-9_]+)*$ // /^[A-Z]{1}[A-Za-z]{2,25}$/;
-    
-        if(!patt.test(value.replace(/\s/g,''))){
+    const checkFirstLastName = (value) => {
+        let patt = /[A-Za-z]{3,25}$/;
+
+        if (!patt.test(value.replace(/\s/g, ''))) {
             return false;
-        }else {
+        } else {
             return true;
         }
     }
 
-    const submit = (e) => {
-        e.preventDefault();
-        console.log(name(form.name)); 
+    const checkEmail = (value) => {
+        let patt = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9.]+)\.([a-zA-Z]{2,6})$/;
+    
+        if(!patt.test(value.trim())){
+            return false;
+        } else {
+            return true;
+        }
+    }
+    const checkText = (value) => {
+        let patt = /^.{5,150}$/;
+
+        if (!patt.test(value.replace(/\s/g, ''))) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    const onChangeInput = (e) => {
         
+        let temporaryForm = { ...form };
+        temporaryForm[e.target.name].value = e.target.value;
+        temporaryForm[e.target.name].isTouched = true;
+        
+        
+        // check input is valid
+        switch (e.target.name) {
+            case 'firstName':
+                temporaryForm[e.target.name].isValid = checkFirstLastName(form.firstName.value);
+                break;
+            case 'lastName':
+                temporaryForm[e.target.name].isValid = checkFirstLastName(form.lastName.value);
+                break;
+            case 'email':
+                temporaryForm[e.target.name].isValid = checkEmail(form.email.value);
+                break;
+            case 'text':
+                temporaryForm[e.target.name].isValid = checkText(form.text.value);
+                break;
+            default:
+                break;
+        }
+
+        // check form is Valid
+        if (temporaryForm['firstName'].isValid &&
+            temporaryForm['lastName'].isValid &&
+            temporaryForm['email'].isValid &&
+            temporaryForm['text'].isValid
+        ){
+            temporaryForm.isValid = true;
+        } else {
+            temporaryForm.isValid = false;
+        }
+
+        // set new state
+        setForm(temporaryForm);
+    }
+
+    const submit = (e) => {
+        
+        e.preventDefault();
+        console.log(form);
     }
 
     return (
@@ -44,23 +133,27 @@ const Contact = () => {
                     </div>
                 </div>
                 <div className="row justify-content-center">
-                    <div className="col-6">
+                    <div className="col-12 col-md-6">
                         <div className="Contact__form">
                             <form>
                                 <h3 className="second-title text-center">Kontakt Forma</h3>
                                 <div className="Contact__form__group">
-                                    <label className="Contact__form__label">Ime i prezime</label>
-                                    <input onChange={(e) => setForm({...form, [e.target.name]: e.target.value})} name="name" value={form.name} type="text" className="Contact__form__group__input" placeholder="Ime i prezime"/>
+                                    <label className="Contact__form__label">Ime</label>
+                                    <input required onChange={onChangeInput} name="firstName" value={form['firstName'].value} type="text" className={form.firstName.isTouched ? form.firstName.isValid ? "Contact__form__group__input" : "Contact__form__group__input Contact__form__group__input-bad" : "Contact__form__group__input"} placeholder="Ime"/>
+                                </div>
+                                <div className="Contact__form__group">
+                                    <label className="Contact__form__label">Prezime</label>
+                                    <input required onChange={onChangeInput} name="lastName" value={form['lastName'].value} type="text" className={form.lastName.isTouched ? form.lastName.isValid ? "Contact__form__group__input" : "Contact__form__group__input Contact__form__group__input-bad" : "Contact__form__group__input"} placeholder="Prezime"/>
                                 </div>
                                 <div className="Contact__form__group">
                                     <label className="Contact__form__label">Email adresa</label>
-                                    <input onChange={(e) => setForm({...form, [e.target.name]: e.target.value})} name="email" value={form.email} type="email" className="Contact__form__group__input" placeholder="Email adresa"/>
+                                    <input required onChange={onChangeInput} name="email" value={form['email'].value} type="email" className={form.email.isTouched ? form.email.isValid ? "Contact__form__group__input" : "Contact__form__group__input Contact__form__group__input-bad" : "Contact__form__group__input"} placeholder="Email adresa"/>
                                 </div>
                                 <div className="Contact__form__group">
                                     <label className="Contact__form__label">Text poruke</label>
-                                    <textarea onChange={(e) => setForm({...form, [e.target.name]: e.target.value})} name="text" value={form.text} type="text" className="Contact__form__group__input-text" cols="30" rows="10" placeholder="Vaša poruka"></textarea>
+                                    <textarea required onChange={onChangeInput} name="text" value={form['text'].value} type="text" className={form.text.isTouched ? form.text.isValid ? "Contact__form__group__input-text" : "Contact__form__group__input-text Contact__form__group__input-bad" : "Contact__form__group__input-text"} cols="30" rows="10" placeholder="Vaša poruka"></textarea>
                                 </div>
-                                <button onClick={submit} className="btn btn-block btn-success">Pošalji poruku</button>
+                                <button disabled={!form.isValid} onClick={submit} className={form.isValid ? "btn btn-block btn-success" : "btn btn-block btn-darkgrey" }>Pošalji poruku</button>
                             </form>
                         </div>
                     </div>
